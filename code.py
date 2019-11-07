13eb5717c8e48cbc56f5ef8f6cf51f76efa36802
taufiqhusada <taufiqhusdr@gmail.com>
2019-11-07 09:56:31 +0700

    coba euy

--- code.py
+++ code.py
@@ -22,7 +22,7 @@ def extract_features(image_path, vector_size=32):
         alg = cv2.KAZE_create()
         # Dinding image keypoints
         kps = alg.detect(image)
-        # Getting first 32 of them. 
+        # Getting first 32 of them.
         # Number of keypoints is varies depend on image size and color pallet
         # Sorting them based on keypoint response value(bigger is better)
         kps = sorted(kps, key=lambda x: -x.response)[:vector_size]
@@ -41,7 +41,7 @@ def extract_features(image_path, vector_size=32):
         print ('Error: ', e)
         return None
     #print(dsc)
-    
+
     return dsc
 
 
@@ -57,7 +57,7 @@ def batch_extractor(images_path):
         #print(name)
         result[name] = extract_features(f)
 
-    
+
 def show_img(path):
     img = imread(path)
     plt.imshow(img)
@@ -70,24 +70,24 @@ def dist(vec1, vec2):
         result+= ((vec1[i])-(vec2[i]))*((vec1[i])-(vec2[i]))
     return math.sqrt(result)
 
-# fungsi cosine similarity 
+# fungsi cosine similarity
 def CosSimilarity(vec1,vec2):
     result = 0.0;
     dotProduct = 0.0;
-    
+
     for i in range(len(vec1)):
         dotProduct+=(vec1[i])*(vec2[i]);
-        
+
     skalarVec1  = 0;
     for e in vec1:
         skalarVec1 += (e)*(e);
     skalarVec1 = math.sqrt(skalarVec1)
-    
+
     skalarVec2  = 0;
     for e in vec2:
         skalarVec2 += (e)*(e);
     skalarVec2 = math.sqrt(skalarVec2)
-    
+
     return dotProduct/(skalarVec1*skalarVec2)
 
 
@@ -119,12 +119,12 @@ def readFromCsv():
             result[key] = temp;
 
 
-pathFolder = input("Masukkan directory yang lengkap:")
+pathFolder = input("Masukkan directory yang lengkap : ")
 
 batch_extractor(pathFolder+'\\Reference')
 saveToCsv();
 
-imageTargetName = input("Masukkan nama file image yang ingin dicompare")
+imageTargetName = input("Masukkan nama file image yang ingin dicompare : ")
 
 imageTarget = pathFolder+'\\Test\\'+imageTargetName
 vectorTarget = extract_features(imageTarget)
@@ -132,7 +132,7 @@ vectorTarget = extract_features(imageTarget)
 
 readFromCsv();
 
-print("Pilihan metode\n1.Cos Similarity\n2.Euclidan Distance")
+print("---Pilihan metode---\n1.Cos Similarity\n2.Euclidan Distance")
 metode = int(input("Masukkan input : "))
 
 resultComparison = []
@@ -146,12 +146,11 @@ elif(metode==2):
         hasil = dist(result[key],vectorTarget)
         resultComparison.append((hasil,key))
     resultComparison.sort();
-    
-show_img(imageTarget)    
 
-print(len(resultComparison))
+show_img(imageTarget)
+
+#print(len(resultComparison))
 for i in range(10):
-    print(resultComparison[i][0]);
-    print(resultComparison[i][1]);
+    #print(resultComparison[i][0]);
+    #print(resultComparison[i][1]);
     show_img(os.path.join(pathFolder,resultComparison[i][1]));
-    
